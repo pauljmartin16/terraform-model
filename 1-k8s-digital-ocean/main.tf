954ed2246a7b88808e7496a0c7baf8715cd7bca4
pauljmartin16 <pauljmartin16@gmail.com>
2024-11-29 12:02:46 -0500

    Update Digital Ocean k8s

--- 1-k8s-digital-ocean/main.tf
+++ 1-k8s-digital-ocean/main.tf
@@ -1,19 +1,19 @@
 
 resource "digitalocean_kubernetes_cluster" "main_cluster" {
-  name   = var.clustername
-  region = "nyc1"
+  name    = var.clustername
+  region  = "nyc1"
   version = "1.31.1-do.4"
 
   node_pool {
     name       = "worker-pool"
     size       = "s-1vcpu-2gb"
-    node_count = 1  
+    node_count = 1
   }
 }
 
 data "digitalocean_kubernetes_cluster" "main_cluster" {
   depends_on = [digitalocean_kubernetes_cluster.main_cluster]
-  name   = var.clustername
+  name       = var.clustername
 }
 
 # ##################################
